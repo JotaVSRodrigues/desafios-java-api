eff59706c9324d546dd92922e032f09d1df30cf8
joaor <joao.rdg119@gmail.com>
2025-09-23 18:31:22 -0300

    commit

--- src/main/java/org/example/exception/InvalidGithubSearch.java
+++ src/main/java/org/example/exception/InvalidGithubSearch.java
@@ -2,7 +2,7 @@ package org.example.exception;
 
 import java.io.IOException;
 
-public class InvalidGithubSearch extends IOException {
+public class InvalidGithubSearch extends RuntimeException {
     public InvalidGithubSearch(String message) {
         super(message);
     }
---
+++ src/main/java/org/example/models/Serializer.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+import com.google.gson.Gson;
+
+public class Serializer {
+
+}
